de8219ff7b6a62daffa68cc6e62bb2ccfdf17140
TimaPilgrim <timatok@gmail.com>
2020-02-04 18:40:57 +0300

    Add strategy to Moxy

--- GoooodNews/app/src/main/java/my/rockpilgrim/goooodnews/ui/information/InfoMvpView.kt
+++ GoooodNews/app/src/main/java/my/rockpilgrim/goooodnews/ui/information/InfoMvpView.kt
@@ -1,5 +1,8 @@
 package my.rockpilgrim.goooodnews.ui.information
 
 import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 
+@StateStrategyType(value = OneExecutionStateStrategy::class)
 interface InfoMvpView : MvpView
--- GoooodNews/app/src/main/java/my/rockpilgrim/goooodnews/ui/list/ListMvpView.kt
+++ GoooodNews/app/src/main/java/my/rockpilgrim/goooodnews/ui/list/ListMvpView.kt
@@ -1,7 +1,10 @@
 package my.rockpilgrim.goooodnews.ui.list
 
 import com.arellomobile.mvp.MvpView
+import com.arellomobile.mvp.viewstate.strategy.OneExecutionStateStrategy
+import com.arellomobile.mvp.viewstate.strategy.StateStrategyType
 
+@StateStrategyType(value = OneExecutionStateStrategy::class)
 interface ListMvpView : MvpView {
     fun dataSuccess()
     fun dataError()
